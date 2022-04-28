00264ed2c4c09425c0235a1b686abe772a3f3436
janaka perera <jcperera.online@gmail.com>
2022-04-29 01:05:10 +1000

    added socials

--- frontend/src/components/SocialMedia.jsx
+++ frontend/src/components/SocialMedia.jsx
@@ -1,17 +1,20 @@
 import React from "react";
-import { BsTwitter, BsInstagram } from "react-icons/bs";
+import { BsTwitter, BsInstagram, BsGithub } from "react-icons/bs";
 import { FaFacebookF } from "react-icons/fa";
 
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <BsTwitter />
+      <BsTwitter onClick={()=> window.open("https://twitter.com/JanakaP65325099", '_blank')}/>
     </div>
     <div>
-      <FaFacebookF />
+      <FaFacebookF onClick={()=> window.open("https://www.facebook.com/jcscorpio", '_blank')}/>
     </div>
     <div>
-      <BsInstagram />
+      <BsInstagram onClick={()=> window.open("https://www.instagram.com/jc.perera", '_blank')}/>
+    </div>
+    <div>
+      <BsGithub onClick={()=> window.open("https://github.com/JcPerera", '_blank')}/>
     </div>
   </div>
 );
